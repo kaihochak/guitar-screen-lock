9048fa538dca1da66dee0c51e48d9eadd314a1a9
Kai Ho Chak <45337569+kaihochak@users.noreply.github.com>
2023-10-17 11:15:10 -0600

    refractored

--- src/script.js
+++ src/script.js
@@ -1,16 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
     console.log('DOM loaded with JavaScript, calling init()');
-    init();
+    currentScreen = 'startScreen';
+    switchScreen();
 });
 
-// current combination of frets, strings, and the right combination
-var fretsClicked = [];
-var stringsClicked = [];
-var correctFrets = ["f2s4", "f3s5", "f2s6"]; // D major
-var correctStrings = ["strum3", "strum4", "strum5", "strum6"]; // D major
-
-// Define a variable to track the current screen
-let currentScreen = 'startScreen';
+// Screens
+const startScreen = document.getElementById('startScreen');
+const guitarScreen = document.getElementById('guitarScreen');
+const unlockScreen = document.getElementById('unlockScreen');
 
 // Strings
 const string1 = document.getElementById('l1');
@@ -20,6 +17,7 @@ const string4 = document.getElementById('l4');
 const string5 = document.getElementById('l5');
 const string6 = document.getElementById('l6');
 
+// Strings
 let strings = {
     strum1: string1,
     strum2: string2,
@@ -29,83 +27,53 @@ let strings = {
     strum6: string6
 }
 
-function switchScreen() {
-    const startScreen = document.getElementById('startScreen');
-    const guitarScreen = document.getElementById('guitarScreen');
-    const unlockedScreen = document.getElementById('unlockedScreen');
+const lockScreenButton = document.getElementById('lockScreen');
 
+// current combination of frets, strings, and the right combination
+var fretsClicked = [];
+var stringsClicked = [];
+// password 
+var correctFrets = ["f2s4", "f3s5", "f2s6"]; // D major
+var correctStrings = ["strum3", "strum4", "strum5", "strum6"]; // D major
+
+// Define a variable to track the current screen
+let currentScreen = 'startScreen';
+
+// Flag to track whether the left mouse button is currently down
+let isLeftMouseDown = false;
+
+function switchScreen() {
     if (currentScreen === 'startScreen') {
+        initSlideUpGesture();
+        resetGuitar();
         startScreen.style.display = 'flex';
         startScreen.style.transform = 'translateY(0)'; // Slide the start screen in
         guitarScreen.style.display = 'none';
         guitarScreen.style.transform = 'translateY(100%)'; // Slide the guitar screen out
-        unlockedScreen.style.transform = 'translateY(100%)'; // Slide the unlocked screen out
+        unlockScreen.style.transform = 'translateY(100%)'; // Slide the unlocked screen out
     } else if (currentScreen === 'guitarScreen') {
+        bindFretButtonsToFunction();
+        bindStringButtonsToFunction();
+        initStrumming();
         guitarScreen.style.transform = 'translateY(0)'; // Slide the guitar screen in
         guitarScreen.style.display = 'flex';
         startScreen.style.transform = 'translateY(-100%)'; // Slide the start screen out
         startScreen.style.display = 'none';
-        unlockedScreen.style.transform = 'translateY(100%)'; // Slide the unlocked screen out
-    } else if (currentScreen === 'unlockedScreen') {
-        startScreen.style.transform = 'translateY(-100%)'; // Slide the start screen out
-        guitarScreen.style.transform = 'translateY(-100%)'; // Slide the guitar screen out
-        unlockedScreen.style.transform = 'translateY(0)'; // Slide the unlocked screen in
+        unlockScreen.style.transform = 'translateY(100%)'; // Slide the unlocked screen out
+    } else if (currentScreen === 'unlockScreen') {
+        initLockScreenButton();
+        unlockScreen.style.transform = 'translateY(0)'; // Slide the unlocked screen in
+        unlockScreen.style.display = 'flex';
+        guitarScreen.style.transform = 'translateY(-100%)'; // Slide the start screen out
+        guitarScreen.style.display = 'none'; // Slide the guitar screen out
     }
 }
 
+// Functions for Start Screen
+
+let startY = null;
+
 function initSlideUpGesture() {
-
-    let startY = null;
-
-    function handleStart(event) {
-        if (event.touches) {
-            // Touch events
-            startY = event.touches[0].clientY;
-        } else {
-            // Mouse events
-            startY = event.clientY;
-        }
-    }
-
-    function handleEnd(event) {
-        if (startY !== null) {
-
-            // if start screen, startY should be at the bottom of the screen
-            // if guitar screen, startY should be at the top of the screen
-            if ((currentScreen === 'startScreen' && startY > window.innerHeight * 5 / 6)
-                || currentScreen === 'guitarScreen' && startY < window.innerHeight * 1 / 6) {
-
-
-                let currentY;
-                if (event.changedTouches) {
-                    // Touch events
-                    currentY = event.changedTouches[0].clientY;
-                } else {
-                    // Mouse events
-                    currentY = event.clientY;
-                }
-
-                // Calculate the distance between the start and current touch/mouse positions
-                // and determine if the user is sliding up to the guitar screen
-                const deltaY = currentY - startY;
-                const screenHeight = window.innerHeight; // Get the screen height
-                const threshold = event.changedTouches ? (screenHeight / 3) : 2;
-
-                if (deltaY < -threshold) { // Adjust the threshold as needed
-                    // Transition to the guitar screen
-                    currentScreen = 'guitarScreen';
-                    switchScreen();
-                } else if (deltaY > threshold) { // Adjust the threshold as needed
-                    // Transition to the start screen
-                    console.log("Transitioning to start screen");
-                    currentScreen = 'startScreen';
-                    switchScreen();
-                }
-                startY = null;
-
-            }
-        }
-    }
 
     // support for touch events
     document.addEventListener('touchstart', handleStart);
@@ -114,6 +82,109 @@ function initSlideUpGesture() {
     // support for mouse events
     document.addEventListener('mousedown', handleStart);
     document.addEventListener('mouseup', handleEnd);
+}
+
+function handleStart(event) {
+    if (event.touches) {
+        // Touch events
+        startY = event.touches[0].clientY;
+    } else {
+        // Mouse events
+        startY = event.clientY;
+    }
+}
+
+function handleEnd(event) {
+    if (startY !== null) {
+
+        // if start screen, startY should be at the bottom of the screen
+        // if guitar screen, startY should be at the top of the screen
+        if ((currentScreen === 'startScreen' && startY > window.innerHeight * 5 / 6)
+            || currentScreen === 'guitarScreen' && startY < window.innerHeight * 1 / 6) {
+
+
+            let currentY;
+            if (event.changedTouches) {
+                // Touch events
+                currentY = event.changedTouches[0].clientY;
+            } else {
+                // Mouse events
+                currentY = event.clientY;
+            }
+
+            // Calculate the distance between the start and current touch/mouse positions
+            // and determine if the user is sliding up to the guitar screen
+            const deltaY = currentY - startY;
+            const screenHeight = window.innerHeight; // Get the screen height
+            const threshold = event.changedTouches ? (screenHeight / 3) : 2;
+
+            if (deltaY < -threshold) { // Adjust the threshold as needed
+                // Transition to the guitar screen
+                currentScreen = 'guitarScreen';
+                switchScreen();
+            } else if (deltaY > threshold) { // Adjust the threshold as needed
+                // Transition to the start screen
+                console.log("Transitioning to start screen");
+                currentScreen = 'startScreen';
+                switchScreen();
+            }
+            startY = null;
+
+        }
+    }
+}
+
+
+// Functions for Guitar Screen
+
+function bindFretButtonsToFunction() {
+    var frets = document.querySelectorAll("button.fret");
+
+    frets.forEach((fret) => {
+        fret.addEventListener('click', onFretClick);
+    });
+}
+
+function bindStringButtonsToFunction() {
+    var strings = document.querySelectorAll("div.string button");
+
+    strings.forEach((string) => {
+        string.addEventListener('mousedown', onStringMouseDown);
+        string.addEventListener('mouseover', onStringMouseOver);
+        string.addEventListener('mouseleave', onStringMouseLeave);
+    });
+}
+
+function initStrumming() {
+
+    // support for touch events
+    document.addEventListener('touchstart', handleStrumStart);
+    document.addEventListener('touchend', handleEnd);
+
+    // support for mouse events
+    document.addEventListener("mousedown", handleStrumStart);
+    document.addEventListener("mouseup", handleStrumEnd);
+}
+
+function handleStrumStart(event) {
+    if (event.touches) {
+        // Touch events
+    } else {
+        // Mouse events
+        if (event.button === 0) isLeftMouseDown = true;
+    }
+}
+
+function handleStrumEnd(event) {
+    if (event.touches) {
+        // Touch events
+    } else {
+        // Mouse events
+        if (event.button === 0 && !event.target.classList.contains('fret')) {
+            isLeftMouseDown = false;
+            verifyChord();
+        }
+    }
 }
 
 function onFretClick() {
@@ -131,35 +202,28 @@ function onFretClick() {
     console.log(fretsClicked); // log current fret combination
 }
 
-function onStringClick() {
+function onStringMouseDown() {
+
     console.log("Clicked string # " + this.id);
     logButtonID(this.id); // Log the ID when the button is clicked
 
-    if (this.style.background != 'red') {
-        this.style.background = 'red'; // change color of strings to indicate being strummed
-        stringsClicked.push(this.id); // add this fret to the input combination
-    } else {
-        this.style.background = ''; // revert color to not strum
-        stringsClicked.pop(this.id); // remove fret from input combination
-    }
-
-    console.log(stringsClicked); // log current string combination
+    strings[this.id].style.width = '5px';
+    strings[this.id].style.background = '#CA3401';
 }
 
-
-function onStringMouseOver() {
+function onStringMouseOver(event) {
     if (event.buttons === 1) { // Check if the left mouse button is clicked
         console.log("Mouse over string # " + this.id);
         logButtonID(this.id); // Log the ID when the button is moused over
+        stringsClicked.push(this.id); // add this fret to the input combination
         strings[this.id].style.width = '5px';
         strings[this.id].style.background = '#CA3401';
-        // this.style.background = '#CA3401'; // change color of string when mouse is clicked
     }
 }
 
-function onStringMouseLeave() {
+function onStringMouseLeave(event) {
     if (event.buttons === 1) { // Check if the left mouse button is clicked
-
+        stringsClicked.push(this.id); // add this fret to the input combination
         setTimeout(() => {
             strings[this.id].style.width = '1px';
             strings[this.id].style.background = '#826352';
@@ -167,30 +231,53 @@ function onStringMouseLeave() {
     }
 }
 
+function arraysAreEqual(arr1, arr2) {
+    // Remove duplicates from the arrays
+    const uniqueArr1 = arr1.filter((value, index, self) => self.indexOf(value) === index);
+    const uniqueArr2 = arr2.filter((value, index, self) => self.indexOf(value) === index);
 
-function logButtonID(id) {
-    console.log("Button ID: " + id);
+    // Sort the unique arrays and then compare them
+    uniqueArr1.sort();
+    uniqueArr2.sort();
+
+    // Check if the arrays are equal
+    return JSON.stringify(uniqueArr1) === JSON.stringify(uniqueArr2);
 }
 
-function bindFretButtonsToFunction() {
-    var frets = document.querySelectorAll("button.fret");
+function verifyChord() {
 
-    frets.forEach((fret) => {
-        fret.addEventListener('click', onFretClick);
-    });
+    const guitar = document.getElementById('guitar');
+
+    if (arraysAreEqual(fretsClicked, correctFrets) && 
+        arraysAreEqual(stringsClicked, correctStrings)) {
+        console.log("correct chord entered . . . unlocking phone");
+        currentScreen = 'unlockScreen';
+        switchScreen();
+    } else {
+        console.log("incorrect chord . . . clearing entry");
+        printChords();
+        resetGuitar();
+        guitar.style.animation = "shake 1s";
+        setTimeout(() => {
+            guitar.style.animation = "";
+        }, 1000);
+    }
+    
+    // fix the bug of strings staying red
+    let stringValues = Object.values(strings);
+    for (let i = 0; i < stringValues.length; i++) {
+        let value = stringValues[i];
+        value.style.width = '1px';
+        value.style.background = '#826352';
+    }
 }
 
-function bindStringButtonsToFunction() {
-    var strings = document.querySelectorAll("div.string button");
-
-    strings.forEach((string) => {
-        string.addEventListener('click', onStringClick);
-        string.addEventListener('mouseover', onStringMouseOver);
-        string.addEventListener('mouseleave', onStringMouseLeave);
-    });
+function printChords() {
+    console.log("Frets: " + fretsClicked);
+    console.log("Strings: " + stringsClicked);
 }
 
-function reset() {
+function resetGuitar() {
     fretsClicked = [];
     stringsClicked = [];
 
@@ -201,40 +288,18 @@ function reset() {
     document.querySelectorAll("div.string button").forEach((string) => {
         string.style.background = '';
     });
+
 }
 
-function unlockPhone() {
-    // Transition to the unlocked screen
-    const guitarScreen = document.getElementById('guitarScreen');
-    const unlockedScreen = document.getElementById('unlockedScreen');
-
-    guitarScreen.style.display = 'none';
-    unlockedScreen.style.display = 'block';
-    currentScreen = 'unlockedScreen';
+function logButtonID(id) {
+    console.log("Button ID: " + id);
 }
 
-function initVerification() {
-    document.querySelector('h1').addEventListener('click', function () {
-        console.log("verifying input chord");
+// Functions for unlock screen
 
-        if (
-            fretsClicked.sort().join(',') === correctFrets.sort().join(',') &&
-            stringsClicked.sort().join(',') === stringsClicked.sort().join(',')
-        ) {
-            console.log("correct chord entered . . . unlocking phone");
-            // TODO: change page to unlocked phone screen
-            document.querySelector('body').style.background = 'green'; //temp
-            unlockPhone();
-        } else {
-            console.log("incorrect chord . . . clearing entry");
-            reset();
-        }
+function initLockScreenButton() {
+    lockScreenButton.addEventListener('click', function () {
+        currentScreen = 'startScreen';
+        switchScreen();
     });
-}
-
-function init() {
-    initSlideUpGesture();
-    bindFretButtonsToFunction();
-    bindStringButtonsToFunction();
-    initVerification();
 }
